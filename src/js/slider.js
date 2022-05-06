354b5a19f767dac515110ac9021c51c4e3e2671a
Yevhen Danilov <danilov.evgen@gmail.com>
2022-05-07 01:10:17 +0300

    portfolio and gallery in progress

---
+++ src/js/slider.js
@@ -0,0 +1,23 @@
+$('.reviews-slider').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  dots: true,
+});
+
+$('.reviews1-slider').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  dots: false,
+});
+
+$('.reviews2-slider').slick({
+  infinite: true,
+  slidesToShow: 5,
+  slidesToScroll: 5,
+  arrows: true,
+  dots: false,
+});
